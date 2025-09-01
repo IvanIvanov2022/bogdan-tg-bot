c54a464d38e28f97a59e29b08c064ac2751d115b
i <ivansvechnikov@gmail.com>
2025-09-01 16:11:08 +0300

    ввёл код

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 import asyncio
+import os
 
 from telebot.async_telebot import AsyncTeleBot
 
-bot = AsyncTeleBot('8161930989:AAHNYo4iKGqeTEOrWlDbArzM1QbvA-tjwOk')
+bot = AsyncTeleBot(os.environ['BOT_TOKEN'])
 
 
 # Handle '/start' and '/help'
