58ec83b24258d7e655e73970031b38e9c62f8690
IvanIvanov2022 <113451759+IvanIvanov2022@users.noreply.github.com>
2025-09-02 07:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 from telebot.async_telebot import AsyncTeleBot
 
-bot = AsyncTeleBot('8161930989:AAHNYo4iKGqeTEOrWlDbArzM1QbvA-tjwOk')
+bot = AsyncTeleBot('')
 
 
 # Handle '/start' and '/help'
